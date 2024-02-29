4db5daef060aebce020f45b417e81d864eada3a9
thinh <anvanthinh@gmail.com>
2024-02-29 23:51:22 +0700

    init

---
+++ src/main/java/com/thinh/serverconfiguration/ServerconfigurationApplication.java
@@ -0,0 +1,13 @@
+package com.thinh.serverconfiguration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerconfigurationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerconfigurationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thinh/serverconfiguration/ServerconfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thinh.serverconfiguration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerconfigurationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
