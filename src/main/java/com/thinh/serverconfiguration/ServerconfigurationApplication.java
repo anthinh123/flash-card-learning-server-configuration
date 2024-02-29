d739c2e6af1ff42986972275a32ea18b0a9ca64c
thinh <anvanthinh@gmail.com>
2024-03-01 00:33:45 +0700

    config to run server configuration

--- src/main/java/com/thinh/serverconfiguration/ServerconfigurationApplication.java
+++ src/main/java/com/thinh/serverconfiguration/ServerconfigurationApplication.java
@@ -2,8 +2,10 @@ package com.thinh.serverconfiguration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class ServerconfigurationApplication {
 
 	public static void main(String[] args) {
